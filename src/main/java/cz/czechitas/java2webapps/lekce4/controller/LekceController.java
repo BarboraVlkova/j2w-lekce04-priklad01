2ca2d15cae8e521519acfbaebd79cf292362ac5c
BarboraVlkova <bvlkova@email.cz>
2021-04-25 12:19:18 +0200

    FINAL verze (s dopsáním kodu od Filipa).

--- src/main/java/cz/czechitas/java2webapps/lekce4/controller/LekceController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce4/controller/LekceController.java
@@ -1,8 +1,18 @@
 package cz.czechitas.java2webapps.lekce4.controller;
 
+import com.fasterxml.jackson.databind.introspect.AnnotationMap;
+import cz.czechitas.java2webapps.lekce4.entity.Lekce;
+import cz.czechitas.java2webapps.lekce4.entity.Pohlavi;
+import cz.czechitas.java2webapps.lekce4.entity.Ucastnik;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Kontroler obsluhující sekci lekcí.
@@ -11,11 +21,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/lekce")
 public class LekceController {
 
-  /**
-   * Metoda kontroleru obsluhující adresu {@code /lekce/}. Používá šablonu {@code /templates/lekce/index.html}.
-   */
-  @GetMapping("/")
-  public String lekce() {
-    return "lekce/index";
-  }
+//  VYTVORIM LIST (seznam(polozky seznamu))
+//  Alt+Enter / Initialize in constructor (nastavit v konstruktoru) - aby mi to nepodtrhavalo cervene
+
+    private final List<Lekce> lekce;
+
+
+//  pridam polozky seznamu / Arrays.asList nebo  List.of s metodou asList a přes Ctrl + P se podívám na nabízené varianty
+//  přidám seznam s lektorama (za posledním parametrem nesmí být čárka (,)
+
+    public LekceController() {
+        lekce = Arrays.asList(
+                new Lekce("Lekce 1", LocalDate.of(2021, 4,21), LocalTime.of(18, 0), LocalTime.of(20, 30), "V mechu"),
+                new Lekce("Lekce 2", LocalDate.of(2021, 4, 28),LocalTime.of(18,0), LocalTime.of(20,30), "V kapradí"),
+                new Lekce("Lekce 3", LocalDate.of(2021, 5, 5),LocalTime.of(18,0), LocalTime.of(20,30), null)
+        );
+    }
+
+
+    /**
+     * Metoda kontroleru obsluhující adresu {@code /lekce/}. Používá šablonu {@code /templates/lekce/index.html}.
+     *
+     * @return
+     */
+    @GetMapping("/")
+    public ModelAndView lekce() {
+        ModelAndView modelAndView = new ModelAndView("lekce/index");
+        modelAndView.addObject("seznamLekci", lekce);
+        return modelAndView;
+    }
+
 }
--- src/main/java/cz/czechitas/java2webapps/lekce4/entity/Lekce.java
+++ src/main/java/cz/czechitas/java2webapps/lekce4/entity/Lekce.java
@@ -12,4 +12,64 @@ public class Lekce {
   private LocalTime casOd;
   private LocalTime casDo;
   private String misto;
+
+
+//  VYGENERUJI PRAZDNY CONSTRUKTOR
+
+  public Lekce() {
+  }
+
+
+//  VYGENERUJI KONSTRUKTOR S VYBRANIM VSEHO
+
+  public Lekce(String nazevLekce, LocalDate datum, LocalTime casOd, LocalTime casDo, String misto) {
+    this.nazev = nazevLekce;
+    this.datum = datum;
+    this.casOd = casOd;
+    this.casDo = casDo;
+    this.misto = misto;
+  }
+
+
+  // VYGENEROVANI GETTERU A SETTERU
+
+  public String getNazev() {
+    return nazev;
+  }
+
+  public void setNazev(String nazev) {
+    this.nazev = nazev;
+  }
+
+  public LocalDate getDatum() {
+    return datum;
+  }
+
+  public void setDatum(LocalDate datum) {
+    this.datum = datum;
+  }
+
+  public LocalTime getCasOd() {
+    return casOd;
+  }
+
+  public void setCasOd(LocalTime casOd) {
+    this.casOd = casOd;
+  }
+
+  public LocalTime getCasDo() {
+    return casDo;
+  }
+
+  public void setCasDo(LocalTime casDo) {
+    this.casDo = casDo;
+  }
+
+  public String getMisto() {
+    return misto;
+  }
+
+  public void setMisto(String misto) {
+    this.misto = misto;
+  }
 }
